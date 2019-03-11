bcdcf6cf2e8f1fcf1f9f3cd2d81d6a1e7d3e8ee8
Eddi3As <eddi3.as@gmail.com>
2019-03-10 22:07:52 -0400

    Ajout de validation
    Ajout de validation au niveau du login et enregistrement input

---
+++ app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents
+  ],
+  imports: [
+    NgbModule.forRoot(),
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ register.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+import {User} from '../user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.styl'],
+  providers:[LoginService]
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+  user: User = {
+    email: '',
+    password: ''
+  };
+  users: Array<User> = [];
+  email: string;
+  password: string;
+
+  constructor(private loginService: LoginService,
+              private router: Router,
+              private formBuilder: FormBuilder) { }
+
+  registerUser() {
+    this.submitted = true;
+ 
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log('email=' +  this.f.email.value);
+    console.log('password=' + this.f.password.value);
+
+    const registeringUser = {
+      email: this.f.email.value,
+      password: this.f.password.value,
+      role: 'user'
+    };
+    console.log(registeringUser);
+/*
+    this.loginService.addUser(registeringUser)
+        .subscribe(res => {
+          this.router.navigateByUrl('/');
+          //  this.loginService.setToken(res['token']);
+          //  this.router.navigateByUrl('/profile');
+          }, err =>{
+            console.log(err._body);
+          //  this.serverErrorMsg = JSON.parse(err._body);
+          //  console.log(this.serverErrorMsg);
+          });*/
+  }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  passwordMatch(){
+    return this.f.password.value === this.f.passwordConfirm.value
+    ? null : {'mismatch': true};
+  }
+
+}
